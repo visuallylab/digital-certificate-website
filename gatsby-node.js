a042e42bc569a7b49274a59570e4eec86c2d87d9
JHIH-CING LI <jhihcing.li@gmail.com>
2020-01-08 11:03:20 +0800

    Disable doc page nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,9 +10,6 @@ const path = require('path');
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
-  // you only want to operate on `Mdx` nodes. If you had content from a
-  // remote CMS you could also check to see if the parent node was a
-  // `File` node here
   if (node.internal.type === 'Mdx') {
     const value = createFilePath({ node, getNode });
 
@@ -55,17 +52,17 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   // Create blog post pages.
   const posts = result.data.allMdx.edges;
 
-  // you'll call `createPage` for each result
-  posts.forEach(({ node }, index) => {
-    createPage({
-      // This is the slug you created before
-      // (or `node.frontmatter.slug`)
-      path: node.fields.slug,
-      // This component will wrap our MDX content
-      component: path.resolve(`./src/components/DocsTemplate.tsx`),
-      // You can use the values in this context in
-      // our page layout component
-      context: { id: node.id },
-    });
-  });
+  // Comment the doc pages because we don't need them for now
+  // posts.forEach(({ node }, index) => {
+  //   createPage({
+  //     // This is the slug you created before
+  //     // (or `node.frontmatter.slug`)
+  //     path: node.fields.slug,
+  //     // This component will wrap our MDX content
+  //     component: path.resolve(`./src/components/DocsTemplate.tsx`),
+  //     // You can use the values in this context in
+  //     // our page layout component
+  //     context: { id: node.id },
+  //   });
+  // });
 };
